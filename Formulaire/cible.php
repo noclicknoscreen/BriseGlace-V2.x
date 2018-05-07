2b413f3dfc8ca18ba906185e7addc769ddf989b8
elisabeth bely <elisabeth.bely@epitech.eu>
2018-05-07 17:35:18 +0200

    Fichier json
    Mets dans le fichier json le nom de l'image si l'upload à reussi

--- Formulaire/cible.php
+++ Formulaire/cible.php
@@ -10,27 +10,6 @@
 <?php
 // header('Content-type: text/javascript');
 
-$jsonFormat = array(
-    "Jeux" => $_POST['jeux'],
-    "mot" => $_POST['mot'],
-    "indice1"=>array(
-        "nom"=>$_POST['indice1'],
-        "photo"=>$_POST['photo1'],
-    ),
-    "indice2"=>array(
-        "nom"=>$_POST['indice2'],
-        "photo"=>$_POST['photo2'],
-    ),
-    "indice3"=>array(
-        "nom"=>$_POST['indice3'],
-        "photo"=>$_POST['photo3'],
-    ),
-    "recompense"=>array(
-        "description"=>$_POST['recompense'],
-        "photo"=>$_POST['photo4'],
-    )
-);
-
 $tmp = $_POST['jeux'];
 if (file_exists ($tmp) == false) {
     mkdir("./" . $tmp, 0777, true);
@@ -49,26 +28,49 @@ if (file_exists ($tmp . "/" . $num . "/image") == false) {
 
 // Photo
 for ($i=1; $i <= 4; $i++) {
-
   $url=$_POST['photo' . $i];
 
   $info = new SplFileInfo($url);
 
   if (urlExist($url)) {
-    $data = file_get_contents($url);
+    $data = @file_get_contents($url);
 
     if ($info->getExtension() == "jpg" || $info->getExtension() == "jpeg" || $info->getExtension() == "png"|| $info->getExtension() == "gif") {
         $new = $tmp . "/" . $num . '/image/image' . $i . '.' . $info->getExtension();
         file_put_contents($new, $data);
+        $imgName[$i] = 'image' . $i . '.' . $info->getExtension();
       }
       else {
         echo "Echec du Chargement de la photo " . $i . " : ce n'est pas une image !<br>";
+        $imgName[$i] = '';
       }
     }
     else {
       echo "Echec du Chargement de la photo " . $i . " : le lien n'existe pas !<br>";
+      $imgName[$i] = '';
     }
 }
+
+$jsonFormat = array(
+    "Jeux" => $_POST['jeux'],
+    "mot" => $_POST['mot'],
+    "indice1"=>array(
+        "nom"=>$_POST['indice1'],
+        "photo"=>$imgName[1],
+    ),
+    "indice2"=>array(
+        "nom"=>$_POST['indice2'],
+        "photo"=>$imgName[2],
+    ),
+    "indice3"=>array(
+        "nom"=>$_POST['indice3'],
+        "photo"=>$imgName[3],
+    ),
+    "recompense"=>array(
+        "description"=>$_POST['recompense'],
+        "photo"=>$imgName[4],
+    )
+);
 
 $fichier = fopen($tmp . "/" . $num . "/enigme.json", "w+");
 fputs($fichier, $json_string = json_encode($jsonFormat, JSON_PRETTY_PRINT));
