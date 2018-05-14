e11937b1379ce6749230445269f1ea3a89d49034
Blindsp0t <contact@blindsp0t.com>
2018-05-14 16:28:11 +0200

    Boggle UPDATE : new physical model based on ofxBullet. (update project files too -> msaLibs have been removed)

--- OF-Common/scenes/scGame3.cpp
+++ OF-Common/scenes/scGame3.cpp
@@ -8,74 +8,135 @@
 
 #include "scGame3.h"
 
+//--------------------------------------------------------------
 void scGame3::setup(){
     
     scScene::setup();
     ofLogNotice() << "Game 3 : Setup !";
     myTitle = "Game 3 : Boggle";
+
     
-    //setup the 3D scene
-    initScene();
+    //gui
+    gui.setup();
+    gui.add(camPosX.set("camPosX", 0, -1000, 1000));
+    gui.add(camPosY.set("camPosY", 150, -1000, 1000));
+    gui.add(camPosZ.set("camPosZ", 545, -1000, 2000));
+    gui.add(gravity.set("gravity", -250, -500, 250));
+    gui.add(forceAmount.set("forceAmount", ofVec3f(100,200,100), ofVec3f(0,0,0), ofVec3f(300,300,300)));
+    gui.add(angularDamping.set("angularDamping", .98, 0.0, 1.0));
+    gui.add(damping.set("damping", .93, 0.0, 1.0));
+    gui.add(friction.set("friction", .93, 0.0, 1.0));
+    gui.add(drawDebug.set("drawDebug", 0));
+    gui.loadFromFile("gui.xml");
+    
+    //OF
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofBackground( 10, 10, 10);
+    ofEnableAntiAliasing();
+    ofSetBoxResolution(30);
+    
+    //texture
+    texture.load("contreplaque.png");
+    
+    //camera
+    camera.setPosition(ofVec3f(camPosX, camPosY, camPosZ));
+
+    //physics
+    world.setup();
+    world.enableDebugDraw();
+    world.setCamera(&camera);
+    world.setGravity(ofVec3f(0,50,0));
+   
+        ground.create( world.world,     ofVec3f(0, 0, 0),       0., 1000.f, 1.f, 1000.f );
+        ground.setProperties(.25, .55);
+        ground.add();
+        
+        rightFace.create( world.world,  ofVec3f(450, 0, 0),    0., 1, 1000, 1000 );
+        rightFace.setProperties(.25, .95);
+        rightFace.add();
+        
+        leftFace.create( world.world,  ofVec3f(-400, 0, 0),    0., 1, 1000, 1000 );
+        leftFace.setProperties(.25, .95);
+        leftFace.add();
+        
+        bottom.create( world.world,  ofVec3f(0, 0, -500),    0., 1000, 1000, 1 );
+        bottom.setProperties(.25, .95);
+        bottom.add();
+    
     
     // Player manager events
     ofAddListener(myPlayerManager->someoneSpoke,this,&scGame3::someoneSpoke);
     
-    //test particles ;)
-    //createTestParticules();
-    
+   
     //TODO : word to be taken from enigma Singleton
     wantedWord = "BONHEUR";
-    createParticules(wantedWord);
+
 };
 
 
+//--------------------------------------------------------------
 void scGame3::update(float dt){
+  
+    camera.setPosition(ofVec3f(camPosX, camPosY, camPosZ));
+    world.setGravity(ofVec3f(0,gravity, 0));
+
+    world.update();
     
-    width = ofGetWidth();
-    height = ofGetHeight();
-    
-    world->update();
+    for(int i=0; i<myCubes.size(); i++)
+    {
+        myCubes[i]->setActivationState(1);
+    }
 };
 
+//--------------------------------------------------------------
 void scGame3::draw(){
     
-    
+    ofEnableAntiAliasing();
+    ofEnableSmoothing();
     ofEnableAlphaBlending();
     ofEnableDepthTest();
-    ofPushMatrix();
     
-    ofTranslate(width/2, 0, -width / 3);	 // center scene
-    ofRotate(rotX, 0, 1, 0);                 // rotate scene
-    ofRotate(rotY, 1, 0, 0);                 // rotate scene
+    ofSetBackgroundColor(255);
     
-    //applyRandomForces();  //makes the screen and walls move with forces applied
-    drawRoom();
-    drawSprings();
+    camera.begin();
     
-    for(int i=0; i<myParticules.size(); i++) {
-        Particle3D_ptr p = myParticules[i]->me;
-        myParticules[i]->draw();
+    if(drawDebug)
+    {
+        ofSetColor(100, 100, 100);
+        ofNoFill();
+        ground.draw();
+        rightFace.draw();
+        leftFace.draw();
+        bottom.draw();
+        ofFill();
     }
     
-    ofPopMatrix();
+    for(int i=0; i<myCubes.size(); i++)
+    {
+        myCubes[i]->customDraw();
+    }
+    
+    if(drawDebug)
+        world.drawDebug();
+    
+    
+    camera.end();
+    
+    
     ofDisableDepthTest();
-    ofDisableLighting();
-    
-    
-    ofPushStyle();
-    // Style setup
-    ofSetColor(ofColor::blue);
-    
-    //scScene::draw();            // Draw title
-    myPlayerManager->draw();    // Draw players
-    drawCenterText(myPlayerManager->getLastMessage(), 0, 50); // Draw last message
-    
-    ofPopStyle();
+    gui.draw();
     
 };
 
+//--------------------------------------------------------------
+void scGame3::exit()
+{
+    gui.saveToFile("gui.xml");
+}
 
-// Speaking event
+
+//--------------------------------------------------------------
 void scGame3::someoneSpoke(player & _player){
     scScene::someoneSpoke(_player);
    
@@ -85,22 +146,36 @@ void scGame3::someoneSpoke(player & _player){
     }
     else
     {
-        addRandomForce(FORCE_AMOUNT * 3);
+        //addRandomForce(FORCE_AMOUNT * 3);
     }
     
 }
 
-//scene notifications
+//--------------------------------------------------------------
 void scGame3::sceneWillAppear( ofxScene * fromScreen ){
+    
     scScene::sceneWillAppear(fromScreen);
-    // reset our scene when we appear
-    setupLighting();
+    
+    for(int i=0; i<wantedWord.size(); i++)
+    {
+        box = new specialBox();
+        box->setup(texture, ofToString(wantedWord[i]), 40);
+        box->create(world.world, ofVec3f(0, 600, 0), .5, 80, 80, 80);
+ 
+        box->add();
+        box->applyForce(START_FORCE_FACTOR*ofVec3f(ofRandom(-1, 1), ofRandom(-1, 1), ofRandom(-1, 1)), box->getPosition());
+        
+        myCubes.push_back(box);
+    }
 };
 
-//scene notifications
+//--------------------------------------------------------------
 void scGame3::sceneWillDisappear( ofxScene * toScreen ){
-}
+};
 
+
+
+//--------------------------------------------------------------
 string scGame3::toUpperCase(string str)
 {
     string strUpper = "";
@@ -110,218 +185,42 @@ string scGame3::toUpperCase(string str)
         strUpper += toupper( str[ i ] );
     }
     return strUpper;
-}  
-
-
-
+};
 
 
 //--------------------------------------------------------------
 void scGame3::keyPressed(int key){
-    
-    switch(key) {
-        case ' ': myParticules[0]->me->makeFree(); break;
-        case 't':
-        {
-            for(int i=0; i<myParticules.size(); i++)
-                myParticules[i]->me->makeFree();
-            
-            break;
-        }
-        case 'f': addRandomForce(FORCE_AMOUNT); break;
-        case 'F': addRandomForce(FORCE_AMOUNT * 3); break;
-            
-        case 'm': myParticules[0]->me->moveTo(ofVec3f(-400,100,100)); myParticules[0]->me->makeFixed(); break;
-            
-        case 's':
-        {
-            
-            for(int i=0; i<myParticules.size(); i++)
-            {
-                particule* ppp = new particule(world, 1, myParticules[i]->getInitPos(), "S");
-                ppp->me->makeFixed();
-                ppp->me->enablePassiveCollision();
-                //myParticules.push_back(ppp);
-                world->makeSpring(ppp->me, myParticules[i]->me, SPRING_MIN_STRENGTH, 300);
-            }
-            break;
-        }
-            
-        case 'S':
-        {
-            for(int i=0; i<world->numberOfSprings(); i++)
-            {
-                world->getSpring(i)->kill();
-            }
-            break;
-        }
-    }
-}
 
-
-//--------------------------------------------------------------
-void scGame3::initScene() {
-    
-    ofBackground(255, 255, 255);
-    ofSetVerticalSync(true);
-    ofSetFrameRate(60);
-    
-    width = ofGetWidth();
-    height = ofGetHeight();
-    
-    world = World3D::create();
-    world->setGravity(ofVec3f(0, GRAVITY, 0));
-    world->setWorldSize(ofVec3f(-width/2, -height, -width/(2*depthDivide)), ofVec3f(width/2, height, width/(2*depthDivide)));
-    world->setSectorCount(SECTOR_COUNT);
-    world->enableCollision();
-}
-
-//--------------------------------------------------------------
-void scGame3::setupLighting() {
-    
-    GLfloat mat_shininess[] = { 50.0 };
-    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light_position[] = { 0, (float)height/2, 0.0, 0.0 };
-    glShadeModel(GL_SMOOTH);
-    
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHT0);
-}
-
-
-//--------------------------------------------------------------
-void scGame3::drawRoom()
-{
-    glDisable(GL_LIGHTING);
-    glBegin(GL_QUADS);
-    // draw right wall
-    glColor3f(.9, 0.9, 0.9);		glVertex3f(width/2, height+1, width/2);
-    glColor3f(1, 1, 1);				glVertex3f(width/2, -height, width/2);
-    glColor3f(0.95, 0.95, 0.95);	glVertex3f(width/2, -height, -width/2);
-    glColor3f(.85, 0.85, 0.85);		glVertex3f(width/2, height+1, -width/2);
-    
-    // back wall
-    glColor3f(.9, 0.9, 0.9);		glVertex3f(width/2, height+1, -width/(2*depthDivide));
-    glColor3f(1, 1, 1);				glVertex3f(width/2, -height, -width/(2*depthDivide));
-    glColor3f(0.95, 0.95, 0.95);	glVertex3f(-width/2, -height, -width/(2*depthDivide));
-    glColor3f(.85, 0.85, 0.85);		glVertex3f(-width/2, height+1, -width/(2*depthDivide));
-    
-    // left wall
-    glColor3f(.9, 0.9, 0.9);		glVertex3f(-width/2, height+1, -width/2);
-    glColor3f(1, 1, 1);				glVertex3f(-width/2, -height, -width/2);
-    glColor3f(0.95, 0.95, 0.95);	glVertex3f(-width/2, -height, width/2);
-    glColor3f(.85, 0.85, 0.85);		glVertex3f(-width/2, height+1, width/2);
-    
-    // front wall
-    /*
-     glColor3f(0.95, 0.95, 0.95);	glVertex3f(width/2, -height, width/2);
-     glColor3f(.85, 0.85, 0.85);		glVertex3f(width/2, height+1, width/2);
-     glColor3f(.9, 0.9, 0.9);		glVertex3f(-width/2, height+1, width/2);
-     glColor3f(1, 1, 1);				glVertex3f(-width/2, -height, width/2);
-     */
-    
-    // floor
-    glColor3f(.8, 0.8, 0.8);
-    glVertex3f(width/2, height+1, width/2);
-    glVertex3f(width/2, height+1, -width/2);
-    glVertex3f(-width/2, height+1, -width/2);
-    glVertex3f(-width/2, height+1, width/2);
-    glEnd();
-    
-}
-
-
-//--------------------------------------------------------------
-void scGame3::drawSprings()
-{
-    // draw springs
-    ofSetColor(128);
-    for(int i=0; i<world->numberOfSprings(); i++) {
-        Spring3D_ptr spring     = world->getSpring(i);
-        Particle3D_ptr a        = spring->getA();
-        Particle3D_ptr b        = spring->getB();
-        ofVec3f vec             = b->getPosition() - a->getPosition();
-        float dist              = vec.length();
-        float angle             = acos( vec.z / dist ) * RAD_TO_DEG;
-        if(vec.z <= 0 ) angle   = -angle;
-        float rx                = -vec.y * vec.z;
-        float ry                =  vec.x * vec.z;
-        
-        ofPushMatrix();
-        ofTranslate(a->getPosition());
-        ofRotate(angle, rx, ry, 0.0);
-        float size  = ofMap(spring->getStrength(), SPRING_MIN_STRENGTH, SPRING_MAX_STRENGTH, SPRING_MIN_WIDTH, SPRING_MAX_WIDTH);
-        
-        ofScale(size, size, dist);
-        ofTranslate(0, 0, 0.5);
-        ofDrawBox(1);
-        ofPopMatrix();
-    }
-}
-
-//--------------------------------------------------------------
-void scGame3::createTestParticules()
-{
-    particule* pp = new particule(world, 100, ofVec3f(-400,-250,100), "S");
-    pp->me->makeFixed();
-    myParticules.push_back(pp);
-    
-    particule* pp2 = new particule(world, 100, ofVec3f(-200,-250,100), "A" );
-    pp2->me->makeFixed();
-    myParticules.push_back(pp2);
-    
-    particule* pp3 = new particule(world, 100, ofVec3f(0,-250,100), "L");
-    pp3->me->makeFixed();
-    myParticules.push_back(pp3);
-    
-    particule* pp4 = new particule(world, 100, ofVec3f(200,-250,100), "U");
-    pp4->me->makeFixed();
-    myParticules.push_back(pp4);
-    
-    particule* pp5 = new particule(world, 100, ofVec3f(400,-250,100), "T");
-    pp5->me->makeFixed();
-    myParticules.push_back(pp5);
-    
-}
-
-//--------------------------------------------------------------
-void scGame3::createParticules(string _word)
-{
-    for (int i=0; i<_word.size(); i++)
+    if(key==' ')
     {
-        particule* pp = new particule(world, 100, ofVec3f(ofRandom(-ofGetWidth()/2, ofGetWidth()/2),-250,100), ofToString(_word[i]));
-        pp->me->makeFree();
-        
-        //adjust mass and bounce
-        pp->me->setMass(15);
-        pp->me->setBounce(0.7);
-        
-        myParticules.push_back(pp);
+        for(int i=0; i<myCubes.size(); i++)
+        {
+            //myCubes[i]->applyTorque(0.0, 1.0, 0.0);
+            ofVec3f randomForce = ofVec3f(ofRandom(-forceAmount->x, forceAmount->x), ofRandom(-forceAmount->y, forceAmount->y), ofRandom(-forceAmount->z, forceAmount->z));
+            myCubes[i]->applyForce(randomForce, /*box->getPosition()*/ofVec3f(-500,0,0));
+        }
     }
-}
+};
 
 
-//--------------------------------------------------------------
-void scGame3::applyRandomForces()
-{
-    if(forceTimer) {
-        float translateMax = forceTimer;
-        glTranslatef(ofRandom(-translateMax, translateMax), ofRandom(-translateMax, translateMax), ofRandom(-translateMax, translateMax));
-        forceTimer--;
-    }
-}
-
-//--------------------------------------------------------------
-void scGame3::addRandomForce(float f) {
-    forceTimer = f;
-    for(int i=0; i<world->numberOfParticles(); i++) {
-        Particle3D_ptr p = world->getParticle(i);
-        if(p->isFree()) p->addVelocity(ofVec3f(ofRandom(-f, f), ofRandom(-f, f), ofRandom(-f, f)));
-    }
-}
-
-
+//
+////--------------------------------------------------------------
+//void scGame3::applyRandomForces()
+//{
+//    if(forceTimer) {
+//        float translateMax = forceTimer;
+//        glTranslatef(ofRandom(-translateMax, translateMax), ofRandom(-translateMax, translateMax), ofRandom(-translateMax, translateMax));
+//        forceTimer--;
+//    }
+//}
+//
+////--------------------------------------------------------------
+//void scGame3::addRandomForce(float f) {
+//    forceTimer = f;
+//    for(int i=0; i<world->numberOfParticles(); i++) {
+//        Particle3D_ptr p = world->getParticle(i);
+//        if(p->isFree()) p->addVelocity(ofVec3f(ofRandom(-f, f), ofRandom(-f, f), ofRandom(-f, f)));
+//    }
+//}
+//
+//
--- OF-Common/scenes/scGame3.h
+++ OF-Common/scenes/scGame3.h
@@ -8,24 +8,87 @@
 
 #pragma once
 #include "scScene.h"
-
-#include "MSAPhysics3D.h"
-#include "particule.h"
-
-
-using namespace msa::physics;
-
-#define UNITS(s)                (s * ofGetWidth() / 1280.0) // scale to uniform units
-#define FORCE_AMOUNT			UNITS(10)
-#define	SPRING_MIN_STRENGTH		0.005
-#define SPRING_MAX_STRENGTH		0.1
-#define	GRAVITY					1
-#define SECTOR_COUNT			1		// currently there is a bug at sector borders, so setting this to 1
-#define	SPRING_MIN_WIDTH		UNITS(1)
-#define SPRING_MAX_WIDTH		UNITS(3)
+#include "ofxGui.h"
+#include "ofxBullet.h"
 
 
-#define depthDivide 2
+#define START_FORCE_FACTOR 100
+#define PLAY_FORCE_FACTOR  7
+
+class specialBox : public ofxBulletBox
+{
+    string letter;
+    ofImage texture;
+    ofTrueTypeFont font;
+    
+public:
+    void customDraw()
+    {
+        transformGL();
+        ofVec3f size = getSize();
+        
+        //apply texture
+        ofEnableNormalizedTexCoords();
+        texture.bind();
+        
+        //3d box
+        ofSetColor(255);
+        
+        ofFill();
+        ofDrawBox(0, 0, 0, size.x, size.y, size.z);
+
+        //contour
+        ofNoFill();
+        ofSetLineWidth(2);
+        ofSetColor(0);
+        ofDrawBox(0, 0, 0, size.x, size.y, size.z);
+        ofFill();
+        
+        texture.unbind();
+        ofDisableNormalizedTexCoords();
+        
+        
+        //letter
+        float textHeight, textWidth;
+        textWidth = font.getStringBoundingBox(letter, 0, 0).getWidth();
+        textHeight = font.getStringBoundingBox(letter, 0, 0).getHeight();
+        
+        ofSetColor(0);
+        ofPushMatrix();
+        ofRotate(180, 0,0,1);
+        ofTranslate(-textWidth/2.0,  -textHeight/2, float(size.z/2 +1));
+        
+        font.drawString(letter, 0, 0);
+        
+        ofPopMatrix();
+
+        //restore gl state
+        restoreTransformGL();
+        
+        
+        
+        
+    }
+    
+    void setLetter(string _letter)
+    {
+        letter = _letter;
+    }
+    
+    void setTexture(ofImage _texture)
+    {
+        texture = _texture;
+    }
+    
+    void setup(ofImage _image, string _letter, float _size)
+    {
+        setTexture(_image);
+        setLetter(_letter);
+        font.load("Avenir.ttc", 34);
+        
+        //setSize(_size);
+    }
+};
 
 
 class scGame3 : public scScene{
@@ -41,41 +104,33 @@ public:
     void sceneWillAppear( ofxScene * fromScreen );
     void sceneWillDisappear( ofxScene * fromScreen );
     
-    
-    ////////////////////////////////////////////
-    void initScene();
-    void setupLighting();
-    void addRandomForce(float f);
-    void drawRoom();
-    void drawSprings();
-    void applyRandomForces();
-    void createTestParticules();
-    void createParticules(string _word);
-    string toUpperCase(string str);
-
-    
     void keyPressed(int key);
-    
+    void exit();
     
     //utils
-    float				rotX, rotY		= 0;
-    int             	width;
-    int         		height;
-    float               forceTimer;
+    string toUpperCase(string str);
     
-    //physics
-    World3D_ptr         world;
-    Attraction3D_ptr    attract;
+    ofxBulletWorldRigid			world;
+    ofxBulletBox				ground, rightFace, leftFace, bottom;
+    specialBox*                 box;
     
-    //the big list
-    vector<particule*>   myParticules;
-    
-    //mouse (view rotation)
-    ofPoint mouseClickPos;
+    //camera
+    //ofEasyCam					camera;
+    ofCamera					camera;
     
     //the word to find
     string wantedWord;
+    
+    //texture
+    ofImage texture;
 
-
+    //the cubes
+    vector<specialBox*>          myCubes;
+    
+    //parameters
+    ofxPanel                     gui;
+    ofParameter<float>           camPosX, camPosY, camPosZ, gravity, angularDamping, damping, friction;
+    ofParameter<ofVec3f>         forceAmount;
+    ofParameter<bool>            drawDebug;
 };
 
