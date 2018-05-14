17efea1ca052ba38a796bbb75146028dbb5a4af0
Blindsp0t <contact@blindsp0t.com>
2018-05-14 19:36:46 +0200

    écran "Hint" => correction du bug d'affichage du text + intégration dans le projet

--- OF-Common/scenes/scHint.h
+++ OF-Common/scenes/scHint.h
@@ -8,6 +8,10 @@
 
 #pragma once
 #include "scScene.h"
+#include "ofMain.h"
+#include "cube.h"
+#include "ofxTextSuite.h"
+
 
 class scHint : public scScene{
 
@@ -21,5 +25,44 @@ public:
     void sceneWillAppear( ofxScene * fromScreen );
     void sceneWillDisappear( ofxScene * fromScreen );
     
+    void drawTexturedCube(int i, float texCoordX_min, float texCoordX_max, float texCoordY_min, float texCoordY_max);
+    void drawTexturedCubeTEXT(int i, float texCoordX_min, float texCoordX_max, float texCoordY_min, float texCoordY_max);
+    void setRevealMode(){revealMode = true;}
+    
+    vector<cube> myCubes;
+
+    
+private :
+    //cubes attributes
+    float cubeSize;
+    float totalWidth;
+    float yPos;
+    
+    //image to be revealed
+    ofImage texture;
+    
+    //utils
+    bool revealMode;
+    int cubeIndex;
+    
+    //font
+    ofTrueTypeFont font;
+    
+    //fbo to draw text, to be used as texture
+    ofFbo textFbo;
+    ofImage textImageFromFbo;
+    
+    //lumiere
+    ofLight spotLight;
+    ofColor lightColor;
+    ofColor materialColor;
+    ofMaterial material;
+    
+    //text
+    ofxTextBlock myText;
+    
+    //timer
+    float timer;
+
 };
 
