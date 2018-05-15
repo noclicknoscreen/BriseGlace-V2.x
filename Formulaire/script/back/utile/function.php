0c05b675d3aef820c8b92d1351a18ea4ee2b4698
elisabeth bely <elisabeth.bely@epitech.eu>
2018-05-15 12:26:43 +0200

    Modification général
    Liste des modifications :
    
    - Organisation des fichiers et dossiers (.php, script, front, back etc.)
    - Structure des énigmes
    - Nom de 2 jeux :
                 Labyrinthe -> Boggle
                 Rebu -> Memory

---
+++ Formulaire/script/back/utile/function.php
@@ -0,0 +1,44 @@
+<?php
+
+// Fonction
+
+//Verification des IMAGES
+
+//Renvoie "True" ou "False"
+function verif_image($image)
+{
+  $url=$image;
+  $info = new SplFileInfo($url);
+  if (urlExist($url)) {
+    $data = @file_get_contents($url);
+    if ($info->getExtension() == "jpg" || $info->getExtension() == "jpeg" || $info->getExtension() == "png"|| $info->getExtension() == "gif" || $info->getExtension() == "JPG") { return true; }
+      else { return false; }
+    }
+    else { return false; }
+}
+
+// Affiche les messages d'erreur concernant les images
+function error_mess($image, $nbimage)
+{
+  $url=$image;
+  $info = new SplFileInfo($url);
+  if (urlExist($url)) {
+    $data = @file_get_contents($url);
+    if ($info->getExtension() == "jpg" || $info->getExtension() == "jpeg" || $info->getExtension() == "png"|| $info->getExtension() == "gif" || $info->getExtension() == "JPG") {
+        return true;
+      }
+      else { echo "Echec du Chargement de la image " . $nbimage . " : ce n'est pas une image !<br>"; }
+    }
+    else { echo "Echec du Chargement de la image " . $nbimage . " : le lien n'existe pas !<br>"; }
+}
+
+// Verifie si le lien existe
+function urlExist($url) {
+    $file_headers = @get_headers($url);
+    if($file_headers[0] == 'HTTP/1.1 404 Not Found')
+       return false;
+
+    return true;
+}
+
+?>
---
+++ Formulaire/script/front/new.php
@@ -0,0 +1,55 @@
+<!-- Input MOT -->
+<p>
+    <label for="mot">Mot à trouver</label> : <br>
+    <input type="text" name="mot" id="mot" value="<?php if (isset($_POST['mot'])){echo $_POST['mot'];} ?>">
+</p>
+
+<details open>
+<summary>ÉDITER</summary>
+<!-- Input INDICE 1 -->
+<p>
+    <label for="indice1">Indice 1</label> : <br>
+    <input type="text" name="indice1" id="indice1" value="<?php if (isset($_POST['indice1'])){echo $_POST['indice1'];} ?>">
+</p>
+<!-- Input IMAGE INDICE 1 -->
+<p>
+    <label for="image1">image indice 1</label> : <br>
+    <input type="url" name="image1" id="image1" class="url_input" value="<?php if (isset($_POST['image1'])){echo $_POST['image1'];} ?>">
+</p>
+<!-- Input INDICE 2 -->
+<p>
+    <label for="indice2">Indice 2</label> : <br>
+    <input type="text" name="indice2" id="indice2" value="<?php if (isset($_POST['indice2'])){echo $_POST['indice2'];} ?>">
+</p>
+<!-- Input IMAGE INDICE 2 -->
+<p>
+    <label for="image2">image indice 2</label> : <br>
+    <input type="url" name="image2" id="image2" class="url_input" value="<?php if (isset($_POST['image2'])){echo $_POST['image2'];} ?>">
+</p>
+<!-- Input INDICE 3 -->
+<p>
+    <label for="indice3">Indice 3</label> : <br>
+    <input type="text" name="indice3" id="indice3" value="<?php if (isset($_POST['indice3'])){echo $_POST['indice3'];} ?>">
+</p>
+<!-- Input IMAGE INDICE 3 -->
+<p>
+    <label for="image3">image indice 3</label> : <br>
+    <input type="url" name="image3" id="image3" class="url_input" value="<?php if (isset($_POST['image3'])){echo $_POST['image3'];} ?>">
+</p>
+<!-- Input RECOMPENSE -->
+<p>
+    <label for="recompense">Recompense</label> : <br>
+    <input type="text" name="recompense" id="recompense" value="<?php if (isset($_POST['recompense'])){echo $_POST['recompense'];} ?>">
+</p>
+<!-- Input image RECOMPENSE -->
+<p>
+    <label for="image4">image Recompense</label> : <br>
+    <input type="url" name="image4" id="image4" class="url_input" value="<?php if (isset($_POST['image4'])){echo $_POST['image4'];} ?>">
+</p>
+<!-- Input legende RECOMPENSE -->
+<p>
+    <label for="legende">Légende</label> : <br>
+    <input type="text" name="legende" id="legende" class="legende_input" value="<?php if (isset($_POST['legende'])){echo $_POST['legende'];} ?>">
+</p>
+</details>
+<p> --------------------------------------------------------------------------------------------------------------------</p>
