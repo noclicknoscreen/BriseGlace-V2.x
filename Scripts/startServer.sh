c610306c04a69aba9fb01f2d74515c8976c3cab7
DudleyDmith <vj.dudley.smith@gmail.com>
2018-06-05 17:19:25 +0200

    start server

--- Scripts/startServer.sh
+++ Scripts/startServer.sh
@@ -1,10 +1,10 @@
 # Run chromium start
-cd "../STTServer"
+#cd "../STTServer"
 
 # nohup node GoogleSpeech_FileServer.js &
 sleep 1
-gnome-terminal --window-with-profile="Term" -e "node SttServer.js"
+node /Users/medenagan-id-001/BriseGlace/OF_0.9.8/apps/BriseGlace-V2.x/STTServer/SttServer.js
 
 # Restart raspberries
-sleep 1
-ssh pi@192.168.1.103 'echo | sudo shutdown -r now'
+#sleep 1
+#ssh pi@192.168.1.103 'echo | sudo shutdown -r now'
