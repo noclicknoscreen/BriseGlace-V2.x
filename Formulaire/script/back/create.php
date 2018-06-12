1d8a2ffa5fc517f549d9b2643e32bea4121c8395
elisabeth bely <elisabeth.bely@epitech.eu>
2018-06-12 17:33:10 +0200

    enigme update auto

--- Formulaire/script/back/create.php
+++ Formulaire/script/back/create.php
@@ -169,4 +169,23 @@ echo "Echec de l'enregistrement.";
 
 $reponse->closeCursor(); // Termine le traitement de la requête
 
+// COPIE DES ENIGMES DANS LE BON DOSSIER
+$dir_source = 'enigme';
+$dir_dest = '../_enigmasNewForm/enigme';
+
+if (!file_exists($dir_dest)) {mkdir($dir_dest, 0755);}
+
+$dir_iterator = new RecursiveDirectoryIterator($dir_source, RecursiveDirectoryIterator::SKIP_DOTS);
+
+$iterator = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
+
+foreach($iterator as $element){
+
+   if($element->isDir()){
+      mkdir($dir_dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+   } else{
+      copy($element, $dir_dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+   }
+}
+
 ?>
