e98ace37f5230c2b523a1e35c7cf87032363eba2
DudleyDmith <vj.dudley.smith@gmail.com>
2019-02-11 11:13:48 +0100

    auto commit logs

--- Scripts/stopAllAndReboot.sh
+++ Scripts/stopAllAndReboot.sh
@@ -1,3 +1,3 @@
-
+/Users/medenagan-id-001/BriseGlace/OF_0.9.8/apps/BriseGlace-V2.x/Scripts/commitLogs.sh
 /Users/medenagan-id-001/BriseGlace/OF_0.9.8/apps/BriseGlace-V2.x/Scripts/stopGod.sh
 echo "Amadeus-1756" | sudo -S /sbin/shutdown -r now
